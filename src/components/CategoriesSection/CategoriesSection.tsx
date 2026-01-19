3b14e8ece2b160a587e1abbc09cede5677e2ca31
Vyshnav S <vyshnav@Vyshnavs-Air-2.bbrouter>
2026-01-19 09:54:26 +0530

    Replaced the h1 tag in the category with h3

--- src/components/CategoriesSection/CategoriesSection.tsx
+++ src/components/CategoriesSection/CategoriesSection.tsx
@@ -18,7 +18,7 @@ export default function CategoriesSection({
                         className={`${styles.categoryBtn} ${selectedCategory === category ? styles.active : ''}`}
                         onClick={() => onCategoryChange(category)}
                     >
-                        <h1 className={styles.category_name} >{category}</h1>
+                        <h3 className={styles.category_name} >{category}</h3>
                     </button>
                 ))}
             </div>
